5c088b132fc64eb784a0ac5da94073aee78f45bb
Michael <michael.ehson@gmail.com>
2025-07-30 04:01:28 +0100

    more auth stuff

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,23 +1,23 @@
 import { Elysia } from "elysia";
 
 const authRouter = new Elysia({ prefix: "/auth" })
-	.post("signup", (ctx) => {
+	.post("/signup", (ctx: any) => {
 		console.log("ctx ===", ctx.body);
 
-		ctx.set.headers["Cookie"] = "Your-secret-cookie-shhhhh";
-
-		if (Math.random() >= 0.5) {
-			return "<p>There is an error!</p>";
-		}
+		ctx.set.headers = { Cookie: "Your-secret-cookie-shhhhh" };
+		ctx.set.headers = { "HX-Redirect": "/app" };
 
 		return "<p>Signing you in!</p>";
 	})
-	.post("login", (ctx) => {
-		console.log("ctx ===", ctx);
+	.post("/login", (ctx: any) => {
+		console.log("GOT HERE!!!");
+
+		ctx.set.headers = { Cookie: "Your-secret-cookie-shhhhh" };
+		ctx.set.headers = { "HX-Redirect": "/app" };
 
 		return {
 			success: true,
-			user: { id: 1, name: "John" },
+			user: { id: 1 },
 		};
 	});
 
